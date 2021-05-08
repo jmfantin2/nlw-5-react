1d8f837f32ac87402ad7408a58d9fcc85e764a52
JM Fantin <joao.fantin@acad.pucrs.br>
2021-05-08 16:21:56 -0300

    static site generation is on

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes);
+
   return (
     <>
       <h1>index!</h1>
     </>
-  )
+  );
+}
+/** getServerSideProps() 
+ *  SSR magic stuff | \/ SSG alt
+*/
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8 //in seconds
+    // generates a new version of the page every 8h
+    // which means we now need a production build to effectively watch this happening
+  }
 }
