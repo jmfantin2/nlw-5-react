5bec96173efb23983c0cce179d5736f4708a6454
JM Fantin <joao.fantin@acad.pucrs.br>
2021-05-11 16:25:29 -0300

    latestEpisodes and allEpisodes diff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,41 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+import styles from './home.module.scss';
+
 type Episode = {
   id: string;
+  url: string;
   title: string;
-  thumbnail: string;
-  description: string;
   members: string;
   duration: number;
+  thumbnail: string;
+  description: string;
+  publishedAt: string;
   durationAsString: string;
-  url: string;
-  publishedAr: string;
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
   return (
-    <div>
-      <h1>index!</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <a href="">{episode.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}/>
     </div>
   );
 }
@@ -46,21 +60,25 @@ export const getStaticProps: GetStaticProps = async () => {
   const episodes = data.map(episode => {
     return {
       id: episode.id,
+      url: episode.file.url,
       title: episode.title,
-      thumbnail: episode.thumbnail,
       members: episode.members,
-      publishedAt: format(parseISO(episode.published_at),'d MMM yy',{locale: ptBR}),
       duration: Number(episode.file.duration),
-      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      thumbnail: episode.thumbnail,
       description: episode.description,
-      url: episode.file.url
+      publishedAt: format(parseISO(episode.published_at),'d MMM yy',{locale: ptBR}),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
     };
   })
+
+  const latestEpisodes = episodes.slice(0,2);
+  const allEpisodes = episodes.slice(2, episodes.length);
 
   /* Make the data available at your own terms */
   return {
     props: {
-      episodes,
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8 //in seconds
     // generates a new version of the page every 8 hours
